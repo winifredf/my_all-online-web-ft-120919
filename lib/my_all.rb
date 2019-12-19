03e77fe86ff8531253d030377058f387b67a53bf
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 04:50:28 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,26 @@
 require 'pry'
 
 def my_all?(collection)
+  index = 0
+  
+  while index < collection.length
+    if yield(collection[index]) == false
+      return false
+    end
+    
+    index += 1
+  end
+  
+  true
 
+end
+
+collection = [1, 2, 3]
+
+my_all?(collection) do |1|
+  1 < 3
+end
+
+my_all?(collection) do |1|
+  1 < 4
 end
