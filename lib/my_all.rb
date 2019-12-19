989745dc0e3a11395a83d7ad8f928810cd7926cf
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 04:52:04 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,26 @@
 require 'pry'
 
 def my_all?(collection)
+  index = 0
+  
+  while index < collection.length
+    if yield(collection[index]) == false
+      return false
+    end
+    
+    index += 1
+  end
+  
+  true
 
+end
+
+collection = [1, 2, 3]
+
+my_all?(collection) do |i|
+  i < 3
+end
+
+my_all?(collection) do |i|
+  i < 4
 end
